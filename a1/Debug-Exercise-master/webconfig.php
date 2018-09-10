9906968040ee811210c51179e6cbb841883607cf
Brehmly <31944827+Brehmly@users.noreply.github.com>
2018-09-09 23:49:29 -0400

    Assignment 1

---
+++ a1/Debug-Exercise-master/webconfig.php
@@ -0,0 +1,5 @@
+<?php
+// added URL Root and Absolute Path stuff
+define('ABSOLUTE_PATH', '/home/acbrehm/htdocs/CIT313/FA2018/a1/Debug-Exercise-master');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:23611/CIT313/FA2018/a1/Debug-Exercise-master');
+?>
