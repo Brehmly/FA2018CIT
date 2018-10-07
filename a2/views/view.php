9c9e743abbcf868ed290b29246a5cae39801d051
Brehmly <31944827+Brehmly@users.noreply.github.com>
2018-10-06 22:52:06 -0400

    Final a2

--- a2/views/view.php
+++ a2/views/view.php
@@ -10,11 +10,11 @@
 
 
     echo "<ul>";
-    echo "<li> UserID: $userID </li>";
-    echo "<li> First Name: $firstName </li>";
-    echo "<li> Last Name: $lastName </li>";
-    echo "<li> Email: $email </li>";
-    echo "<li> Role: $role </li>";
+    echo "<li> UserID: ". $userID . " </li>";
+    echo "<li> First Name: ". $firstName . " </li>";
+    echo "<li> Last Name: ". $lastName . " </li>";
+    echo "<li> Email: ". $email . " </li>";
+    echo "<li> Role: ". $role . " </li>";
     echo "</ul>";
   ?>
 </body>
